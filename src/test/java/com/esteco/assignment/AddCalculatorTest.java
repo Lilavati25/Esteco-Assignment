c8348b15e91748bfd7fb4eddf38684289cea83d1
Lilavati Shivaji Mhaske <mhaskelilavati4545@gmail.com>
2023-05-23 23:19:42 +0100

    Added tests: to verify negative number support, multiple errors, unknown arguments support of add() of AddCalculator class

--- src/test/java/com/esteco/assignment/AddCalculatorTest.java
+++ src/test/java/com/esteco/assignment/AddCalculatorTest.java
@@ -1,9 +1,29 @@
+/**
+ * This class contains unit tests for the AddCalculator class, which provides functionality to perform addition operations
+ * on a set of numbers with various input scenarios.
+ *
+ * @author Lilavati Shivaji Mhaske
+ */
 package com.esteco.assignment;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddCalculatorTest {
+
+    /**
+     * Test case to verify the behavior of the add() method when handling an unknown number of arguments.
+     */
+    @Test
+    public void testAddMethod_UnknownNumberOfArguments() {
+        String numbersSet1 = "1,2,3,4,5";
+        String numbersSet2 = "5,10,15,20,25";
+        String numbersSet3 = "10,20,30,40,50";
+
+        String result = AddCalculator.add(numbersSet1, numbersSet2, numbersSet3);
+
+        assertEquals("15\n75\n150", result);
+    }
 
     /*
      * This test verifies the behavior of the add() method for valid input when handling the newline character as a delimiter.
@@ -47,18 +67,44 @@ public class AddCalculatorTest {
         String numbers4 = "//|\n1|2,3";
         String expectedErrorMessage = "'|' expected but ',' found at position 3.";
 
-        // Perform the tests
         String result1 = AddCalculator.add(numbers1);
         String result2 = AddCalculator.add(numbers2);
         String result3 = AddCalculator.add(numbers3);
         String result4 = AddCalculator.add(numbers4);
 
-        // Verify the results
         assertEquals("3", result1);
         assertEquals("6", result2);
         assertEquals("5", result3);
         assertEquals(expectedErrorMessage, result4);
     }
 
+    /**
+     * Test case to verify the behavior of the add() method when handling negative numbers.
+     */
+    @Test
+    public void testAddMethod_NegativeNumbers() {
 
+        String numbers1 = "-1,2";
+        String numbers2 = "2,-4,-5";
+        String expectedErrorMessage = "Negative not allowed : -4, -5";
+
+        String result1 = AddCalculator.add(numbers1);
+        String result2 = AddCalculator.add(numbers2);
+
+        assertEquals("Negative not allowed : -1", result1);
+        assertEquals(expectedErrorMessage, result2);
+    }
+
+    /**
+     * Test case to verify the behavior of the add() method when encountering multiple errors.
+     */
+    @Test
+    public void testAddMethod_MultipleErrors() {
+        String numbers = "-1,,-2";
+        String expectedErrorMessage = "Negative not allowed : -1\nNumber expected but ',' found at position 3\nNegative not allowed : -2";
+
+        String result = AddCalculator.add(numbers);
+
+        assertEquals(expectedErrorMessage, result);
+    }
 }
